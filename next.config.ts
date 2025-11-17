c4a6f4753a5c583c4d29da4a3ea637143ba11f99
Shamar Morrison <shamar.webdev@gmail.com>
2025-11-17 12:44:12 -0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  experimental: {
+    typedEnv: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
